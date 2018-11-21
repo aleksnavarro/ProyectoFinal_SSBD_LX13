df6d357e2698f7866c2a30abd874c8085f7c6054
aleksnavarro <jairo.navarros@alumnos.udg.mx>
2018-11-21 14:59:32 -0600

    primer commit

---
+++ API/contacto.py
@@ -0,0 +1,27 @@
+class Contacto:
+    def __init__(self,conexion,cursor):
+        self.conexion=conexion
+        self.cursor=cursor
+
+    def recuperar(self):
+        contactos=[]
+        select=("SELECT *FROM Contactos")
+        self.cursor.execute(select,)
+        for i in self.cursor.fetchall():
+            inv={
+                'cont_id':i[0],
+                'nombre':i[1],
+                'telefono':i[2],
+                'correo':i[3],
+                'fb':i[4],
+                'tw':i[5],
+                'ig':i[6],
+                'notas':i[7]
+            }
+            contactos.append(inv)
+
+        return contactos
+
+    def agregar(self):
+
+        return 0
---
+++ API/servidor.py
@@ -0,0 +1,25 @@
+from flask import Flask,request,make_response,jsonify
+import mysql.connector
+from contacto import Contacto
+
+conexion=mysql.connector.connect(user="alejandro",password="12345",database="LibretaContactos")
+cursor=conexion.cursor()
+
+app = Flask(__name__)
+
+@app.route("/home/")
+def hello():
+    #return "Hello World!"
+    respuesta=make_response("hello world")
+    respuesta.headers.add('Access-Control-Allow-Origin','*')
+    return respuesta
+
+@app.route("/contactos/",methods=['GET'])
+def contacto():
+    cto=Contacto(conexion,cursor)
+    resultado=cto.recuperar()
+    print(resultado)
+
+    return jsonify(resultado)
+
+app.run(debug=True)
